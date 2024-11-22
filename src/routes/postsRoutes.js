893e376fe73cd90b05eef7d344c32592a493c9e7
Handy Claude Marie Milliance <milliance23@gmail.com>
2024-11-22 12:05:42 -0300

    feat: atualizado

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -4,8 +4,15 @@ import {
   ListPosts,
   postarNovoPost,
   uploadImagem,
+  atualizarNovoPost,
 } from "../controllers/postsController.js";
 import multer from "multer";
+import cors from "cors";
+
+const corsOptions = {
+  origin: "http://localhost:8000",
+  optionsSuccessStatus: 200,
+};
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -22,7 +29,7 @@ const routes = (app) => {
   // devolver json para o front-end
   // Habilita o parsing de JSON no corpo das requisições
   app.use(express.json());
-
+  app.use(cors(corsOptions));
   // Rota para obter todos os posts
   app.get("/posts", ListPosts);
 
@@ -34,6 +41,8 @@ const routes = (app) => {
 
   // Rota para upload de imagens
   app.post("/upload", upload.single("imagem"), uploadImagem);
+
+  app.put("/upload/:id", atualizarNovoPost);
 };
 
 export default routes;
